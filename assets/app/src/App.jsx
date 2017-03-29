54c2682a4e51f3147c127a508d9169ccf3331cad
Whitney DeSpain <whitneymdespain@gmail.com>
2017-03-29 17:23:19 -0600

    adds list page functionality

--- assets/app/src/App.jsx
+++ assets/app/src/App.jsx
@@ -13,6 +13,7 @@ import LoginPage from './components/LoginPage.jsx';
 import Footer from './components/Footer.jsx';
 import SearchPage from './components/SearchPage.jsx';
 import BillPage from './components/BillPage.jsx';
+import ListPage from './components/ListPage.jsx';
 
 var listStyle = {
     display: 'inline',
@@ -58,6 +59,7 @@ var App = React.createClass({
                         <Route path="/login" component={LoginPage} />
                         <Route path="/search" component={SearchPage} />
                         <Route path="/bill" component={BillPage} />
+                        <Route path="/list" component={ListPage} />
                         <Route component={NotFound} />
                     </Switch>
                     {/*<Footer className="page-footer" />*/}
--- assets/app/src/components/HomePage.jsx
+++ assets/app/src/components/HomePage.jsx
@@ -22,8 +22,10 @@ var HomePage = React.createClass({
         </div>
         <div className="col l6 offset-l3">
             <p className="flow-text">Ratemybill.com is the demystifier of Utah legislation that allows users to discuss bills, view trends, and voice opinions.</p>
+            <p className="flow-text"><Link to="/list"> Click here to view the list of bills </Link></p>
             <Link to="/signup"><span className="waves-effect waves-light btn">Create account</span></Link>
         </div>
+
       </div>
       <div className="row container">
       <Link to="/bill">
